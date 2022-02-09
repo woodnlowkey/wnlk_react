3b0a7963533da5ab199e9c1b262b125919e29dae
unknown <woodnlowkey>
2022-02-09 14:04:13 +0900

    'effect'

--- react/react-app/src/App.js
+++ react/react-app/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Button";
+import styles from "./App.module.css";
+import {useState, useEffect} from "react";
 
 function App() {
+  const [keyword, setKeyword] = useState("")
+  const onChange = (event) => setKeyword(event.target.value);
+  useEffect(() => {
+    if (keyword.length > 1) {
+      console.log("SEARCH FOR", keyword);
+    }
+  }, [keyword]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search"/>
+      <h1 className={styles.title}>React</h1>
+      <Button text={"Continue"}/>
     </div>
   );
 }
