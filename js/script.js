f755770af4fa7d9b5f1b8653e200ce7e8ffd6ada
unknown <woodnlowkey>
2022-02-09 11:50:55 +0900

    'nodejs'

--- js/script.js
+++
@@ -1,9 +0,0 @@
-const button = document.querySelector('#click');
-
-function clickFunction(event) {
-    event.preventDefault();
-    console.log('click');
-    button.value = Number(button.value) + 1;
-}
-
-button.addEventListener('click', clickFunction);
