3da43394242a4a86c1c7f8fbbea15265cc0dcbed
unknown <woodnlowkey>
2022-02-09 14:57:16 +0900

    'recapState'

--- react/react-app/src/App.js
+++ react/react-app/src/App.js
@@ -1,22 +1,25 @@
-import Button from "./Button";
-import styles from "./App.module.css";
-import {useState, useEffect} from "react";
+import { useState } from "react";
 
 function App() {
-  const [keyword, setKeyword] = useState("")
-  const onChange = (event) => setKeyword(event.target.value);
-  useEffect(() => {
-    if (keyword.length > 1) {
-      console.log("SEARCH FOR", keyword);
-    }
-  }, [keyword]);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value)
+  const onSubmit = (event) => {
+    event.preventDefault(); 
+    if (toDo === "") {return;}
+    setToDos((curArr) => [toDo, ...curArr]);
+    setToDo("");
+  };
+  console.log(toDos);
   return (
-    <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search"/>
-      <h1 className={styles.title}>React</h1>
-      <Button text={"Continue"}/>
-    </div>
+  <div>
+    <h1>My To Dos {toDos.length}</h1>
+    <form onSubmit={onSubmit}>
+      <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do!" />
+      <button>Add To Do</button>
+    </form>
+  </div>
   );
-}
+};
 
 export default App;
